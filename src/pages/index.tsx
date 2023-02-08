375bc2af067ce00dcce3d7186e7a7121f41fc4fc
Rudra Patel <84099324+imrudrapatel@users.noreply.github.com>
2023-02-08 10:59:49 +0530

    Final changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,36 +4,40 @@ import databaseData from "../database/data.json";
 import type { Data, Location } from "../types/data";
 
 export default function Home({ locations }: { locations: Location[] }) {
-    return (
+  return (
+    <div className="grid grid-cols-4 gap-y-16">
+      {locations.map((location) => (
         <>
-            {locations.map((location) => (
-                <>
-                    <Link href={`/location/${location.id}`}>
-                        <Image
-                            src={location.image}
-                            alt={`${location.location} Map`}
-                            width={50}
-                            height={50}
-                        />
-                        <h3 key={location.id}>{location.location}</h3>
-                        <p>Interested: {location.interested}</p>
-                    </Link>
-                </>
-            ))}
+          <Link href={`/location/${location.id}`}>
+            <div>
+              <div>
+                <Image
+                  src={location.image}
+                  alt={`${location.location} Map`}
+                  width={300}
+                  height={300}
+                />
+              </div>
+              <h1 key={location.id}>{location.location}</h1>
+              <p>Interested: {location.interested}</p>
+            </div>
+          </Link>
         </>
-    );
+      ))}
+    </div>
+  );
 }
 
 export async function getServerSideProps() {
-    const data: Data[] = databaseData;
-    const locations: Location[] = data.map((d: Data) => {
-        return {
-            id: d.id,
-            location: `${d.city}, ${d.state}`,
-            image: d.image,
-            interested: d.interested,
-        };
-    });
+  const data: Data[] = databaseData;
+  const locations: Location[] = data.map((d: Data) => {
+    return {
+      id: d.id,
+      location: `${d.city}, ${d.state}`,
+      image: d.image,
+      interested: d.interested,
+    };
+  });
 
-    return { props: { locations } };
+  return { props: { locations } };
 }
