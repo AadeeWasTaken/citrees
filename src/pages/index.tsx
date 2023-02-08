66e75e6b6d5b17ade6223606c0d4d0c17e392f9f
Aadeetya <aadeetyapatil@gmail.com>
2023-02-07 21:15:36 -0500

    data go brrrrrrrrrrrr

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
+import databaseData from "../database/data.json";
+import type { Data, Location } from "../types/data";
+
 export default function Home() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+}
+
+export async function getServerSideProps() {
+    const data: Data[] = databaseData;
 }
---
+++ src/types/data.ts
@@ -0,0 +1,14 @@
+export type Data = {
+    city: string;
+    state: string;
+    image: string;
+    soil: string;
+    temprature: number;
+    tree: string;
+}
+
+export type Location = {
+    city: string;
+    state: string;
+    image: string;
+}
